247cdc74b7ad7cfdae14865af02cdea344b715eb
Muhammad Hadi Saeed <hadisaeed067@gmail.com>
2022-07-08 15:30:29 +0500

    User Service Updated

--- UserService/src/main/java/com/user/service/UserService.java
+++ UserService/src/main/java/com/user/service/UserService.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class UserService {
@@ -14,7 +16,8 @@ public class UserService {
 
     public Response addUser(User user) {
         try {
-
+            userRepository.save(user);
+            return new Response("User Added Successfully", HttpStatus.CREATED, null);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -23,7 +26,12 @@ public class UserService {
 
     public Response getUser(int id) {
         try {
-
+            Optional<User> user = userRepository.findById(id);
+            if (user.isPresent()) {
+                return new Response("User Record Found", HttpStatus.FOUND, user.get());
+            } else {
+                return new Response("User Record Not Found", HttpStatus.NOT_FOUND, null);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -32,7 +40,8 @@ public class UserService {
 
     public Response updateUser(User user) {
         try {
-
+            userRepository.save(user);
+            return new Response("User Updated Successfully", HttpStatus.OK, null);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -41,7 +50,13 @@ public class UserService {
 
     public Response deleteUser(int id) {
         try {
-
+            Optional<User> user = userRepository.findById(id);
+            if (user.isPresent()) {
+                userRepository.deleteById(id);
+                return new Response("User Deleted Successfully", HttpStatus.OK, null);
+            } else {
+                return new Response("User Record Not Found", HttpStatus.NOT_FOUND, null);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
